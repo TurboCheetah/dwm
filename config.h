b0fa1b4290b8cef440d3e9725978e37b3af6c125
Turbo <dev@turbo.ooo>
2022-03-21 09:44:01 -0500

    change color names

--- config.h
+++ config.h
@@ -11,11 +11,11 @@ static const char col_gray1[]       = "#161320";
 static const char col_gray2[]       = "#161320";
 static const char col_gray3[]       = "#D9E0EE";
 static const char col_gray4[]       = "#161320";
-static const char col_cyan[]        = "#DDB6F2";
+static const char col_mauve[]        = "#DDB6F2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_mauve, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray1, col_mauve,  col_mauve  },
 };
 
 /* tagging */
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_mauve, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
